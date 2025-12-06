aabbc87b56a0950848d3892c99692572a6a751fa
Felipe U <felipe_uv@outlook.com>
2025-12-06 15:55:41 -0500

    feat: Update Experience page content and style timeline

--- src/pages/Exp.tsx
+++ src/pages/Exp.tsx
@@ -1,13 +1,39 @@
+import '../styles/Exp.css'
+
+const experience = [
+  {
+    id: 1,
+    occupation: 'Arquitecto Desarrollador',
+    company: 'Ceiba Software (Clienste Sura)',
+    date: 'Nov 2022 - Nov 2023',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumquesit pariatur vitae consequuntur, cupiditate maxime minus reiciendis ipsa eius exercitationem ipsam. Accusamus corrupti enimdoloremque culpa quidem itaque incidunt minima?',
+  },
+  {
+    id: 2,
+    occupation: 'Practicante Arquitecto Desarrollador',
+    company: 'Ceiba Software (Clienste Sura)',
+    date: 'May 2022 - Oct 2022',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumquesit pariatur vitae consequuntur, cupiditate maxime minus reiciendis ipsa eius exercitationem ipsam. Accusamus corrupti enimdoloremque culpa quidem itaque incidunt minima?',
+  },
+]
+
 export function ExpPage() {
   return (
     <div className='exp-container'>
       <h2>Experiencia</h2>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates
-        eaque quasi voluptatem, assumenda tenetur eos corporis a, molestiae quae
-        nobis omnis illum ipsam blanditiis maxime commodi earum iste facere
-        officiis?
-      </p>
+      <div className='timeline'>
+        {experience.map((exp) => (
+          <div key={exp.id} className='timeline-item'>
+            <p className='timeline-date'>{exp.date}</p>
+            <div className='timeline-dot' />
+            <div className='timeline-job'>
+              <h3>{exp.occupation}</h3>
+              <h4>{exp.company}</h4>
+              <p>{exp.desc}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }

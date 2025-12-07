1f410cb23aeaf2011b20c205bdf7547a95a48fae
Felipe U <felipe_uv@outlook.com>
2025-12-07 14:43:32 -0500

    feat: Update Projects page content and structure

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,13 +1,77 @@
+import '../styles/Projects.css'
+
 export function ProjectsPage() {
   return (
     <div className='projects-container'>
       <h2>Proyectos</h2>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates
-        eaque quasi voluptatem, assumenda tenetur eos corporis a, molestiae quae
-        nobis omnis illum ipsam blanditiis maxime commodi earum iste facere
-        officiis?
-      </p>
+
+      <div className='projects'>
+        <div className='project'>
+          <img
+            src='https://i.pinimg.com/1200x/76/75/42/767542303972a837a7fa4d2cf26bf5f5.jpg'
+            alt='Random Image'
+          />
+          <div className='project-desc'>
+            <h3>Project Name</h3>
+            <p>
+              Project description Lorem ipsum dolor sit amet consectetur
+              adipisicing elit
+            </p>
+            <div className='project-tags'>
+              <span>HTML</span>
+              <span>React</span>
+              <span>TypeScript</span>
+            </div>
+            <p>
+              <a href='#'>Go to website</a>
+            </p>
+          </div>
+        </div>
+
+        <div className='project'>
+          <img
+            src='https://i.pinimg.com/1200x/76/75/42/767542303972a837a7fa4d2cf26bf5f5.jpg'
+            alt='Random Image'
+          />
+          <div className='project-desc'>
+            <h3>Project Name</h3>
+            <p>
+              Project description Lorem ipsum dolor sit amet consectetur
+              adipisicing elit
+            </p>
+            <div className='project-tags'>
+              <span>HTML</span>
+              <span>React</span>
+              <span>TypeScript</span>
+            </div>
+            <p>
+              <a href='#'>Go to website</a>
+            </p>
+          </div>
+        </div>
+
+        <div className='project'>
+          <img
+            src='https://i.pinimg.com/1200x/76/75/42/767542303972a837a7fa4d2cf26bf5f5.jpg'
+            alt='Random Image'
+          />
+          <div className='project-desc'>
+            <h3>Project Name</h3>
+            <p>
+              Project description Lorem ipsum dolor sit amet consectetur
+              adipisicing elit
+            </p>
+            <div className='project-tags'>
+              <span>HTML</span>
+              <span>React</span>
+              <span>TypeScript</span>
+            </div>
+            <p>
+              <a href='#'>Go to website</a>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }

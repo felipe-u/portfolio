2b33be49c41c694d08c50488786e7e922de4c313
Felipe U <felipe_uv@outlook.com>
2025-12-07 14:57:45 -0500

    feat: Update Skills page and structure

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -1,13 +1,37 @@
+import '../styles/Skills.css'
+import { useState } from 'react'
+
 export function SkillsPage() {
+  const [skillsTab, setSkillsTab] = useState<'tech' | 'soft'>('tech')
+
   return (
     <div className='skills-container'>
       <h2>Habilidades</h2>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates
-        eaque quasi voluptatem, assumenda tenetur eos corporis a, molestiae quae
-        nobis omnis illum ipsam blanditiis maxime commodi earum iste facere
-        officiis?
-      </p>
+
+      <div className='skills-wrap'>
+        <div className='skills-selector'>
+          <h3 onClick={() => setSkillsTab('tech')}>Técnicas</h3>
+          <h3 onClick={() => setSkillsTab('soft')}>Blandas</h3>
+        </div>
+
+        <div className='skills-view'>
+          {skillsTab === 'tech' ? (
+            <ul>
+              <li>React</li>
+              <li>React</li>
+              <li>React</li>
+              <li>React</li>
+            </ul>
+          ) : (
+            <ul>
+              <li>Comunicacion</li>
+              <li>Comunicacion</li>
+              <li>Comunicacion</li>
+              <li>Comunicacion</li>
+            </ul>
+          )}
+        </div>
+      </div>
     </div>
   )
 }

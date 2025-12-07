380d14141c7f6e401118dd34fe117d53e03c327c
Felipe U <felipe_uv@outlook.com>
2025-12-07 15:11:51 -0500

    feat: Update Contact page and structure

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,13 +1,28 @@
+import '../styles/Contact.css'
+
 export function ContactPage() {
   return (
     <div className='contact-container'>
-      <h2>Contacto</h2>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates
-        eaque quasi voluptatem, assumenda tenetur eos corporis a, molestiae quae
-        nobis omnis illum ipsam blanditiis maxime commodi earum iste facere
-        officiis?
-      </p>
+      <div className='contact-info'>
+        <h2>Contacto</h2>
+        <div className='contact-item'>
+          <span>♠</span>
+          <p>felipe_uv@outlook.com</p>
+        </div>
+
+        <div className='contact-item'>
+          <span>♠</span>
+          <p>+57 312 775 1316</p>
+        </div>
+      </div>
+
+      <div className='contact-info'>
+        <h2>Redes</h2>
+        <div className='icons'>
+          <div className='icon'>♠</div>
+          <div className='icon'>♠</div>
+        </div>
+      </div>
     </div>
   )
 }
